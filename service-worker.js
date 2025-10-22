21acb4565b7fd6b46b76ff41ebabd18f244b6927
Joao <joaoestevam.1204@gmail.com>
2025-10-21 22:55:43 -0300

    Atualizações PWA e Icones

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,35 @@
+const CACHE_NAME = 'treino-app-v1';
+const BASE = '/Treino-Tracker/'; 
+const urlsToCache = [
+  BASE,
+  BASE + 'index.html',
+  BASE + 'styles.css',
+  BASE + 'image/imagem_1.png',
+  BASE + 'manifest.json',
+
+];
+
+
 self.addEventListener('install', event => {
-  console.log('Service Worker instalado');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => console.log('Arquivos pré-cacheados com sucesso'))
+  );
 });
 
-const BASE = '/Treino-Tracker/'; // troque pelo nome real
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
 
 self.addEventListener('fetch', event => {
   let url = event.request.url;
@@ -11,5 +38,9 @@ self.addEventListener('fetch', event => {
     url = self.origin + BASE + url.substring(self.origin.length + 1);
   }
 
-  event.respondWith(fetch(url).catch(() => caches.match(event.request)));
+  event.respondWith(
+    caches.match(url)
+      .then(response => response || fetch(url))
+      .catch(() => caches.match(BASE + 'index.html')) 
+  );
 });
