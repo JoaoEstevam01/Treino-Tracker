7b60ee397d32e3b6c6271b0eaffbc89e35bb6795
Joao <joaoestevam.1204@gmail.com>
2025-10-21 22:24:53 -0300

    Tentativa de correção erro 404

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,14 @@ self.addEventListener('install', event => {
   console.log('Service Worker instalado');
 });
 
+const BASE = '/Treino-Tracker/'; // troque pelo nome real
+
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  let url = event.request.url;
+
+  if (url.startsWith(self.origin) && !url.includes(BASE)) {
+    url = self.origin + BASE + url.substring(self.origin.length + 1);
+  }
+
+  event.respondWith(fetch(url).catch(() => caches.match(event.request)));
 });
